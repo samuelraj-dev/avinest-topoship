84cc8ff48a5d58d32bb91ea3eb619a031de6908f
H Samuel Raj <samuelrajholyns@gmail.com>
2026-04-06 14:18:21 +0530

    test cicd 9

--- avinest-infra/vpc.tf
+++ avinest-infra/vpc.tf
@@ -181,14 +181,97 @@ resource "aws_db_instance" "postgres" {
 #   }
 # }
 
-# resource "aws_s3_bucket" "frontend" {
-#   bucket = "avinest-frontend-unique-12345"
-# }
+resource "aws_s3_bucket" "frontend" {
+  bucket = "avinest.topoship.com"
 
-# resource "aws_s3_bucket_website_configuration" "frontend" {
-#   bucket = aws_s3_bucket.frontend.id
+  tags = {
+    Name = "avinest-frontend"
+    Environment = "prod"
+  }
+}
 
-#   index_document {
-#     suffix = "index.html"
-#   }
-# }
+resource "aws_s3_bucket_ownership_controls" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
+
+resource "aws_cloudfront_origin_access_control" "frontend" {
+  name                              = "avinest-oac"
+  description                       = "OAC for S3 frontend"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "frontend" {
+  enabled             = true
+  comment             = "avinest-frontend-cdn"
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
+
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "avinest-frontend-cdn"
+    Env  = "prod"
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.frontend.arn}/*",
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+          }
+        }
+      }
+    ]
+  })
+}
